7422c878c785b75f8e6323ec7b9b1cd45094b980
marie-0405 <sirisuamari0405@yahoo.co.jp>
2021-05-03 20:53:56 +0900

    [add]記事詳細画面

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-   public function getPaginateByLimit(int $max_post = 2){
+   public function getPaginateByLimit(int $max_post = 10){
        # $thisはインスタンス自身を示すから、Postデータを示す
        return $this->orderBy('id', 'DESC')->paginate($max_post);
    }
