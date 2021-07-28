a9916e403d81bde52c30346e44680433c4d4d654
marie-0405 <sirisuamari0405@yahoo.co.jp>
2021-07-28 16:01:03 +0900

    Add:DB::statement

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('posts')->truncate();
+         DB::table('posts')->insert([[
+            'title' => 'title1',
+            'body' => 'This is body1',],
+            ['title' => 'title2',
+            'body' => 'This is body2',]
+            ]
+        );
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    }
+}
